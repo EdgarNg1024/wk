d83a18f0547201210ef464557a040a09f8d14386
EdgarNg1024 <EdgarNg_1024@163.com>
2018-08-21 00:01:44 +0800

    增加老人呼救功能

--- app/src/main/java/edgar/wk/help/activity/HelpActivity.java
+++ app/src/main/java/edgar/wk/help/activity/HelpActivity.java
@@ -3,12 +3,22 @@ package edgar.wk.help.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import edgar.wk.R;
 
 public class HelpActivity extends AppCompatActivity {
+
+    @BindView(R.id.etPerson)
+    protected EditText EtPhoneMembers;
+
+    //紧急联系人们
+    List<String> phoneMembers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
