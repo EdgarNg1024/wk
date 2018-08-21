69c48b81686eefd0d3d18e4bbda7117191aa1781
EdgarNg1024 <EdgarNg_1024@163.com>
2018-08-21 10:15:32 +0800

    tag一下老人求救的处理 -- 发送短信

--- app/src/main/java/edgar/wk/help/activity/HelpActivity.java
+++ app/src/main/java/edgar/wk/help/activity/HelpActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -17,8 +18,14 @@ public class HelpActivity extends AppCompatActivity {
     @BindView(R.id.etPerson)
     protected EditText EtPhoneMembers;
 
+    @BindView(R.id.etMsgContent)
+    protected EditText EtMsgContent;
+
     //紧急联系人们
     List<String> phoneMembers;
+    //消息内容
+    String msgContent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,24 @@ public class HelpActivity extends AppCompatActivity {
     @OnClick({R.id.btnHelp})
     void OnClick(View v) {
         switch (v.getId()) {
-
             case R.id.btnHelp:
+                phoneMembers = Arrays.asList(EtPhoneMembers.getText().toString().split(","));
+                msgContent = EtMsgContent.getText().toString();
+
+                // 获取短信管理器
+                android.telephony.SmsManager smsManager = android.telephony.SmsManager
+                        .getDefault();
+
+                for (String phoneNum : phoneMembers) {
+
+                    // 拆分短信内容（手机短信长度限制）
+                    List<String> divideContents = smsManager.divideMessage(msgContent);
+                    for (String text : divideContents) {
+                        smsManager.sendTextMessage(phoneNum, null, text, null, null);
+                    }
+                }
+
+                //同时按顺序拨打电话
 
                 break;
 
